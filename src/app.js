480e3b806e47c8814b70b7303887f11540552a5f
Jhosehp <jhosehp.rendon@gmail.com>
2018-04-02 13:50:58 -0500

    Add form and event handlers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,51 @@
 console.log('App.js is running');
 
-var template = (
-    <div>
-        <p>Changed This is JSX</p>
-        <ul>
-            <li>Buy food</li>
-            <li>Buy food</li>
-        </ul>
-        <form>
-            <input type="text" name="option"/>
-            <button>Add option</button>
-        </form>    
-    </div>
-);
-var appRoot = document.getElementById('app');
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+}
 
-ReactDOM.render(template, appRoot);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if(option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+const onRemoveAll = () => {
+    app.options = [];
+    render();
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            <h2>{app.subtitle}</h2>
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ul>
+                <li>Buy food</li>
+                <li>Buy food</li>
+            </ul>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>    
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+
+};
+
+render();
